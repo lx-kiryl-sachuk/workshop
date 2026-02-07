f29539b1018e45b7dcdf5c943dfbe823992b69ab
Kiryl Sachuk <ownfrezzy@gmail.com>
2026-02-07 23:15:02 +0100

    fix: restore WS event subscription TODOs in skeleton

--- api/src/token/token.service.ts
+++ api/src/token/token.service.ts
@@ -1,54 +1,44 @@
 import { Inject, Injectable, Logger, OnModuleInit } from "@nestjs/common";
 import Web3 from "web3";
 import ABI from "../abi/WorkshopToken.abi.json";
-import { WEB3_HTTP } from "../web3/web3.constants";
+import { WEB3_HTTP, WEB3_WS } from "../web3/web3.constants";
 import { HistoryStore } from "./history.store";
 
 @Injectable()
 export class TokenService implements OnModuleInit {
   private readonly logger = new Logger(TokenService.name);
   private contractHttp: any;
-  private lastBlock = 0;
-  private pollInterval: ReturnType<typeof setInterval> | null = null;
+  private contractWs: any;
 
   constructor(
     @Inject(WEB3_HTTP) private readonly web3Http: Web3,
+    @Inject(WEB3_WS) private readonly web3Ws: Web3,
     private readonly history: HistoryStore,
   ) {}
 
-  async onModuleInit() {
+  onModuleInit() {
     const tokenAddress = process.env.TOKEN_ADDRESS;
     if (!tokenAddress) throw new Error("TOKEN_ADDRESS is required");
 
     // TODO: Initialize this.contractHttp using:
     //   new this.web3Http.eth.Contract(ABI as any, tokenAddress)
 
-    // TODO: Store the current block number in this.lastBlock:
-    //   this.lastBlock = Number(await this.web3Http.eth.getBlockNumber());
+    // TODO: Initialize this.contractWs the same way but with this.web3Ws
 
-    // TODO: Start polling for Transfer events every 2 seconds:
-    //   this.pollInterval = setInterval(() => this.pollTransferEvents(), 2000);
-  }
-
-  private async pollTransferEvents() {
-    // TODO: Get the latest block number
-    //   const latest = Number(await this.web3Http.eth.getBlockNumber());
-    //   if (latest <= this.lastBlock) return;
-
-    // TODO: Fetch past Transfer events from this.lastBlock+1 to "latest":
-    //   const events = await this.contractHttp.getPastEvents("Transfer", {
-    //     fromBlock: this.lastBlock + 1,
-    //     toBlock: "latest",
-    //   });
-
-    // TODO: Loop through events and push each to this.history:
-    //   for (const ev of events) {
+    // TODO: Subscribe to Transfer events via WebSocket (web3.js v4 API):
+    //   const subscription = this.contractWs.events.Transfer();
+    //
+    //   subscription.on("data", (ev: any) => {
     //     const { from, to, value } = ev.returnValues;
-    //     this.history.push({ txHash: ev.transactionHash, from, to,
-    //       value: value.toString(), blockNumber: Number(ev.blockNumber) });
-    //   }
-
-    // TODO: Update this.lastBlock = latest;
+    //     this.history.push({
+    //       txHash: ev.transactionHash, from, to,
+    //       value: value.toString(), blockNumber: Number(ev.blockNumber),
+    //     });
+    //   });
+    //
+    //   subscription.on("error", (err: any) =>
+    //     this.logger.error("Transfer subscription error", err),
+    //   );
   }
 
   async balanceOf(address: string) {
--- api/src/web3/web3.module.ts
+++ api/src/web3/web3.module.ts
@@ -1,5 +1,5 @@
 import { Module } from "@nestjs/common";
-import { WEB3_HTTP } from "./web3.constants";
+import { WEB3_HTTP, WEB3_WS } from "./web3.constants";
 
 // TODO: import Web3
 
@@ -8,7 +8,11 @@ import { WEB3_HTTP } from "./web3.constants";
     // TODO: Create a provider for WEB3_HTTP
     //   - use `useFactory` to return a new Web3 instance
     //   - connect to process.env.RPC_HTTP (default: "http://127.0.0.1:8545")
+
+    // TODO: Create a provider for WEB3_WS
+    //   - use `useFactory` to return a new Web3 instance with a WebSocket provider
+    //   - use: new Web3(new Web3.providers.WebsocketProvider(process.env.RPC_WS ?? "ws://127.0.0.1:8545"))
   ],
-  exports: [WEB3_HTTP],
+  exports: [WEB3_HTTP, WEB3_WS],
 })
 export class Web3Module {}
