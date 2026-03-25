e40ff8488b0ec22e447a8328666e971997f0cfad
Uladzimir Yubko <uladzimir.yubko@leverx.com>
2026-03-25 19:45:02 +0100

    Clean up

--- api/src/token/token.service.ts
+++ api/src/token/token.service.ts
@@ -1,6 +1,5 @@
 import { Inject, Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import Web3 from 'web3';
-import ABI from '../abi/WorkshopToken.abi.json';
 import { WEB3_HTTP, WEB3_WS } from '../web3/web3.constants';
 import { HistoryStore } from './history.store';
 
@@ -20,46 +19,24 @@ export class TokenService implements OnModuleInit {
     const tokenAddress = process.env.TOKEN_ADDRESS;
     if (!tokenAddress) throw new Error('TOKEN_ADDRESS is required');
 
-    // TODO: Initialize this.contractHttp using:
-    //   new this.web3Http.eth.Contract(ABI as any, tokenAddress)
+    // TODO: Initialize contractHttp and contractWs instances
 
-    // TODO: Initialize this.contractWs the same way but with this.web3Ws
-
-    // TODO: Subscribe to Transfer events via WebSocket (web3.js v4 API):
-    //   const subscription = this.contractWs.events.Transfer();
-    //
-    //   subscription.on("data", (ev: any) => {
-    //     const { from, to, value } = ev.returnValues;
-    //     this.history.push({
-    //       txHash: ev.transactionHash, from, to,
-    //       value: value.toString(), blockNumber: Number(ev.blockNumber),
-    //     });
-    //   });
-    //
-    //   subscription.on("error", (err: any) =>
-    //     this.logger.error("Transfer subscription error", err),
-    //   );
+    // TODO: Subscribe to Transfer events via WebSocket (web3.js v4 API)
   }
 
   async balanceOf(address: string) {
     // TODO: Call this.contractHttp.methods.balanceOf(address).call()
-    // TODO: Return { address, balance: <result as string> }
+
     return { address, balance: '0' };
   }
 
   async transfer(fromPk: string, to: string, amount: string) {
-    // TODO: Derive account from private key:
-    //   const account = this.web3Http.eth.accounts.privateKeyToAccount(fromPk);
-    //   this.web3Http.eth.accounts.wallet.add(account);
+    // TODO: Derive account from private key
 
-    // TODO: Build the transfer transaction:
-    //   const tx = this.contractHttp.methods.transfer(to, amount);
+    // TODO: Build the transfer transaction
 
-    // TODO: Estimate gas and send the transaction:
-    //   const gas = await tx.estimateGas({ from: account.address });
-    //   const receipt = await tx.send({ from: account.address, gas: gas.toString() });
+    // TODO: Estimate gas and send the transaction
 
-    // TODO: Return { from: account.address, to, amount, txHash: receipt.transactionHash }
     return { from: '', to, amount, txHash: '' };
   }
 
--- api/src/web3/web3.module.ts
+++ api/src/web3/web3.module.ts
@@ -6,11 +6,7 @@ import { WEB3_HTTP, WEB3_WS } from './web3.constants';
 @Module({
   providers: [
     // TODO: Create a provider for WEB3_HTTP
-    //   - use `useFactory` to return a new Web3 instance
-    //   - connect to process.env.RPC_HTTP (default: "http://127.0.0.1:8545")
     // TODO: Create a provider for WEB3_WS
-    //   - use `useFactory` to return a new Web3 instance with a WebSocket provider
-    //   - use: new Web3(new Web3.providers.WebsocketProvider(process.env.RPC_WS ?? "ws://127.0.0.1:8545"))
   ],
   exports: [WEB3_HTTP, WEB3_WS],
 })
